23655d466a34aaa17df5aa5487ebec430c48f70c
Bradley M. Kuhn <bkuhn@ebb.org>
2010-10-05 16:07:05 -0400

    Try to avoid .git directory when run from a git checkout version.

--- compile-all.py
+++ compile-all.py
@@ -8,12 +8,13 @@ import os
 def compileDir(arg, directory, names):
     compileall.compile_dir(directory, force=1)
 
-os.path.walk(".", compileDir, "")
+os.path.walk("podjango", compileDir, "")
+compileall.compile_dir(".", force=1)
 
 os.system("find . -type d -exec chmod gu+rx {} \;")
 os.system("find . -type f -exec chmod gu+r {} \;")
 os.system("find . -type d -exec chmod o+x {} \;")
 os.system("find ./podjango/static -type d -exec chmod o+rx {} \;")
 os.system("find ./podjango/static  -exec chmod o+r {} \;")
-os.system("find . -exec chgrp www-data {} \;")
+os.system("find . -exec chgrp .-data {} \;")
 os.system("find . -exec chmod go-w {} \;")
