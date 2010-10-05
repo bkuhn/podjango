e5c01c658dc7306339454013222e5ddf3e86696f
Bradley M. Kuhn <bkuhn@ebb.org>
2010-10-05 17:34:00 -0400

    Fix location of settings.

--- faif_wrapper.py
+++ faif_wrapper.py
@@ -2,7 +2,7 @@
 # so we just bypass it altogether and set the relevant variable here.
 # See deployment documentation for more info.
 from os import environ
-environ["DJANGO_SETTINGS_MODULE"] = 'settings'
+environ["DJANGO_SETTINGS_MODULE"] = 'podjango.settings'
 environ["CANONICAL_HOSTNAME"] = 'faif.us'
 
 from modpythoncustom import *
