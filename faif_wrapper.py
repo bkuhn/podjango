fcff28399d3704564ae59ae361d08d45d06e709d
Bradley M. Kuhn <bkuhn@ebb.org>
2010-10-05 15:47:55 -0400

    Added faif wrapper script.

---
+++ faif_wrapper.py
@@ -0,0 +1,8 @@
+# req.subprocess_env is not set when the PostReadRequestHandler runs,
+# so we just bypass it altogether and set the relevant variable here.
+# See deployment documentation for more info.
+from os import environ
+environ["DJANGO_SETTINGS_MODULE"] = 'settings'
+environ["CANONICAL_HOSTNAME"] = 'faif.us'
+
+from modpythoncustom import *
